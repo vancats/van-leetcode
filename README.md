5c47efb651d76f9957801de0e3d4224fed5f4a6d
Lqf <leiqifan@163.com>
2022-02-17 16:53:36 +0800

    20. 有效的括号

--- README.md
+++ README.md
@@ -67,4 +67,5 @@
   - [682. 棒球比赛](https://leetcode-cn.com/problems/baseball-game/)
   - [844. 比较含退格的字符串](https://leetcode-cn.com/problems/backspace-string-compare/)
   - [946. 验证栈序列](https://leetcode-cn.com/problems/validate-stack-sequences/)
+	- [20. 有效的括号](https://leetcode-cn.com/problems/valid-parentheses/))
 
---
+++ src/栈（处理具有完全包含关系的问题）/20. 有效的括号.js
@@ -0,0 +1,31 @@
+/*
+ * @Author: Lqf
+ * @Date: 2022-02-17 16:53:16
+ * @LastEditors: Lqf
+ * @LastEditTime: 2022-02-17 16:53:17
+ * @Description: 我添加了修改
+ */
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  let stack = []
+  let map = new Map()
+  map.set('(', ')')
+  map.set('[', ']')
+  map.set('{', '}')
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      stack.push(s[i])
+    } else {
+      if (map.get(stack[stack.length - 1]) === s[i]) {
+        stack.pop()
+      } else {
+        return false
+      }
+    }
+  }
+  return stack.length === 0
+}
