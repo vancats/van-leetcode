5850df408f46e8030a7b59d87ccd5d56dc6f5600
Lqf <leiqifan@163.com>
2022-03-01 14:06:41 +0800

    373. 查找和最小的k对数字

--- README.md
+++ README.md
@@ -137,3 +137,4 @@
   - [1046. 最后一块石头的重量](https://leetcode-cn.com/problems/last-stone-weight/)
   - [703. 数据流中的第 K 大元素](https://leetcode-cn.com/problems/kth-largest-element-in-a-stream/)
   - [215. 数组中的第K个最大元素](https://leetcode-cn.com/problems/kth-largest-element-in-an-array/)
+  - [373. 查找和最小的 K 对数字](https://leetcode-cn.com/problems/find-k-pairs-with-smallest-sums/)
---
+++ src/堆/373. 查找和最小的 K 对数字.js
@@ -0,0 +1,81 @@
+/*
+ * @Author: Lqf
+ * @Date: 2022-03-01 13:54:07
+ * @LastEditors: Lqf
+ * @LastEditTime: 2022-03-01 14:04:25
+ * @Description: 我添加了修改
+ */
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k
+ * @return {number[][]}
+ */
+
+class Heap {
+  constructor(sign = true) {
+    this.heap = []
+    // true 是大顶堆，false 是小顶堆
+    this.sign = sign
+  }
+  push (val) {
+    this.heap.push(val)
+    let ind = this.heap.length - 1
+    // 向上整理
+    while (ind > 0 && this.compare(ind, Math.floor((ind - 1) / 2))) {
+      this.swap(ind, Math.floor((ind - 1) / 2))
+      ind = Math.floor((ind - 1) / 2)
+    }
+    return this.heap.length
+  }
+
+  pop () {
+    if (!this.heap.length) return console.log('full')
+    // 堆顶元素出堆，尾部元素放到顶部
+    let val = this.heap.shift()
+    this.heap.length && this.heap.unshift(this.heap.pop())
+
+    // 向下整理
+    let ind = 0, tempInd, leftInd, rightInd
+    while (true) {
+      tempInd = ind
+      leftInd = ind * 2 + 1
+      rightInd = ind * 2 + 2
+      if (this.heap[leftInd] !== undefined && this.compare(leftInd, tempInd)) tempInd = leftInd
+      if (this.heap[rightInd] !== undefined && this.compare(rightInd, tempInd)) tempInd = rightInd
+      if (tempInd === ind) break
+      this.swap(ind, tempInd)
+      ind = tempInd
+    }
+    return val
+  }
+
+  compare (ind1, ind2) {
+    return this.sign ?
+      this.heap[ind1][0] + this.heap[ind1][1] > this.heap[ind2][0] + this.heap[ind2][1]
+      : this.heap[ind1][0] + this.heap[ind1][0] < this.heap[ind2][0] + this.heap[ind2][1]
+  }
+  swap (ind1, ind2) {
+    [this.heap[ind1], this.heap[ind2]] = [this.heap[ind2], this.heap[ind1]]
+  }
+}
+
+var kSmallestPairs = function (nums1, nums2, k) {
+  let heap = new Heap()
+  for (let i = 0; i < nums1.length; i++) {
+    let val1 = nums1[i]
+    for (let j = 0; j < nums2.length; j++) {
+      let val2 = nums2[j]
+      if (heap.heap.length < k) {
+        heap.push([val1, val2])
+        continue
+      }
+      if (heap.heap[0][0] + heap.heap[0][1] <= val1 + val2) break
+      console.log(1)
+      heap.push([val1, val2])
+      heap.pop()
+    }
+  }
+  return heap.heap
+}
