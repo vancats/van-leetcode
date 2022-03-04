8eeb2ff34ea76396350e921eaf16472568ddacd3
Lqf <leiqifan@163.com>
2022-03-04 09:15:04 +0800

    2104. 子数组范围和

--- README.md
+++ README.md
@@ -149,3 +149,4 @@
   - [1753. 移除石子的最大得分](https://leetcode-cn.com/problems/maximum-score-from-removing-stones/)
   - [1801. 积压订单中的订单总数](https://leetcode-cn.com/problems/number-of-orders-in-the-backlog/)
 - 22-3-3 [258. 各位相加](https://leetcode-cn.com/problems/add-digits/)
+- 22-3-4 [2104. 子数组范围和](https://leetcode-cn.com/problems/sum-of-subarray-ranges/)
---
+++ src/栈（处理具有完全包含关系的问题）/2104. 子数组范围和.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: Lqf
+ * @Date: 2022-03-04 09:14:33
+ * @LastEditors: Lqf
+ * @LastEditTime: 2022-03-04 09:14:37
+ * @Description: 还有栈的解法
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var subArrayRanges = function (nums) {
+  let ans = 0
+  for (let i = 0; i < nums.length - 1; i++) {
+    let max = nums[i], min = nums[i]
+    for (let j = i + 1; j < nums.length; j++) {
+      max = Math.max(nums[j], max)
+      min = Math.min(nums[j], min)
+      ans += max - min
+    }
+  }
+  return ans
+}
