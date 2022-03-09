309710386ca92687f326cda0b1c5cd6e06e70b4a
Lqf <leiqifan@163.com>
2022-03-09 14:40:36 +0800

    添加ts+jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: Lqf
+ * @Date: 2022-03-09 11:18:12
+ * @LastEditors: Lqf
+ * @LastEditTime: 2022-03-09 14:21:34
+ * @Description: 我添加了修改
+ */
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  coverageDirectory: 'coverage',
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+}
