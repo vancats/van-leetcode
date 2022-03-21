d73c3ec551238caea69033a812d97ba5d046e4e4
Lqf <leiqifan@163.com>
2022-03-21 16:16:18 +0800

    optimized mergeSort

--- utils/SortMode.ts
+++ utils/SortMode.ts
@@ -3,13 +3,10 @@
  * @return {*}
  * @param {number} nums
  */
-export function quickSort(nums: number[]): number[] {
-  return sortSubArr(nums, 0, nums.length - 1)
-}
-function sortSubArr(nums: number[], left: number, right: number): number[] {
+export function quickSort(nums: number[], left: number = 0, right: number = nums.length - 1): number[] {
   const index = partition(nums, left, right)
-  if (index - 1 > left) sortSubArr(nums, left, index - 1)
-  if (index < right) sortSubArr(nums, index, right)
+  if (index - 1 > left) quickSort(nums, left, index - 1)
+  if (index < right) quickSort(nums, index, right)
   return nums
 }
 function partition(nums: number[], left: number, right: number): number {
@@ -33,24 +30,24 @@ function partition(nums: number[], left: number, right: number): number {
  * @return {*}
  * @param {number} nums
  */
-export function mergeSort(nums: number[]): number[] {
-  if (nums.length < 2) return nums
-  const mid = nums.length >> 1
-  const leftArr = mergeSort(nums.slice(0, mid))
-  const rightArr = mergeSort(nums.slice(mid))
-  nums = mergeSubArr(leftArr, rightArr)
+export function mergeSort(nums: number[], left: number = 0, right: number = nums.length - 1): number[] {
+  if (left >= right) return nums
+  const mid = (left + right) >> 1
+  mergeSort(nums, left, mid)
+  mergeSort(nums, mid + 1, right)
+
+  let i = left, j = mid + 1, k = 0, temp: number[] = []
+  while (i <= mid || j <= right) {
+    if ((j > right) || (i <= mid && nums[i] <= nums[j])) {
+      temp[k++] = nums[i++]
+    } else {
+      temp[k++] = nums[j++]
+    }
+  }
+  for (let i = left; i <= right; i++) nums[i] = temp[i - left]
   return nums
 }
 
-function mergeSubArr(leftArr: number[], rightArr: number[]): number[] {
-  let i = 0, j = 0, result: number[] = []
-  while (i < leftArr.length && j < rightArr.length) {
-    if (leftArr[i] < rightArr[j]) result.push(leftArr[i++])
-    else result.push(rightArr[j++])
-  }
-  if (i < leftArr.length) return result.concat(leftArr.slice(i))
-  else return result.concat(rightArr.slice(j))
-}
 
 
 /**
