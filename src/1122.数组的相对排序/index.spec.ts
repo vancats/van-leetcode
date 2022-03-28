caab2f674f6a62aaf9b374d6b2abf5dc97c433b6
Lqf <leiqifan@163.com>
2022-03-28 14:52:07 +0800

    1122. 数组的相对排序

---
+++ src/1122.数组的相对排序/index.spec.ts
@@ -0,0 +1,15 @@
+import { relativeSortArray } from "./index"
+
+describe('1122.数组的相对排序', () => {
+  it('示例一', () => {
+    const arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19]
+    const arr2 = [2, 1, 4, 3, 9, 6]
+    expect(relativeSortArray(arr1, arr2)).toStrictEqual([2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19])
+  })
+
+  it('示例二', () => {
+    const arr1 = [28, 6, 22, 8, 44, 17]
+    const arr2 = [22, 28, 8, 6]
+    expect(relativeSortArray(arr1, arr2)).toStrictEqual([22, 28, 8, 6, 17, 44])
+  })
+})
---
+++ src/1122.数组的相对排序/index.ts
@@ -0,0 +1,20 @@
+/**
+ * @description: 时间复杂度 O(N) 空间复杂度 O(N)
+ * @return {*}
+ * @param {number} arr1
+ * @param {number} arr2
+ */
+export function relativeSortArray(arr1: number[], arr2: number[]): number[] {
+  let arr = new Array(1001).fill(0), res: number[] = []
+  for (let i = 0; i < arr1.length; i++) {
+    arr[arr1[i]]++
+  }
+  for (let i = 0; i < arr2.length; i++) {
+    while (arr[arr2[i]]--) res.push(arr2[i])
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    while (arr[i]-- > 0) res.push(i)
+  }
+  return res
+}
