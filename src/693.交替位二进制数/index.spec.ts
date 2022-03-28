4427d6bd581be8d59fde24edb0a10b9f99628b9f
Lqf <leiqifan@163.com>
2022-03-28 09:45:38 +0800

    653. 交替位二进制数

---
+++ src/693.交替位二进制数/index.spec.ts
@@ -0,0 +1,15 @@
+import { hasAlternatingBits } from './index'
+
+describe('693.交替位二进制数', () => {
+  it('示例一', () => {
+    expect(hasAlternatingBits(5)).toBe(true)
+  })
+
+  it('示例二', () => {
+    expect(hasAlternatingBits(7)).toBe(false)
+  })
+
+  it('示例三', () => {
+    expect(hasAlternatingBits(11)).toBe(false)
+  })
+})
---
+++ src/693.交替位二进制数/index.ts
@@ -0,0 +1,16 @@
+/**
+ * @description: 时间复杂度 O(lgN) 空间复杂度 O(1)
+ * @return {*}
+ * @param {number} n
+ */
+export function hasAlternatingBits(n: number): boolean {
+  let prev = n % 2 ? 1 : 0
+  n = n >> 1
+  while (n) {
+    let temp = n % 2 ? 1 : 0
+    if (temp === prev) return false
+    prev = temp
+    n = n >> 1
+  }
+  return true
+}
