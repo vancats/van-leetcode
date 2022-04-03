8ca58e14015f44340035522cf45bf510b22776ca
leiqifan <leiqifan@163.com>
2022-04-04 01:10:54 +0800

    307. 区域和检索

---
+++ src/307.区域和检索-数组可修改/index.ts
@@ -0,0 +1,22 @@
+/**
+ * @description: Annotation
+ * @return {*}
+ */
+export class NumArray {
+  arr: number[]
+  constructor(nums: number[]) {
+    this.arr = nums
+  }
+
+  update(index: number, val: number): void {
+    this.arr[index] = val
+  }
+
+  sumRange(left: number, right: number): number {
+    let sum = 0
+    for (let i = left; i <= right; i++) {
+      sum += this.arr[i]
+    }
+    return sum
+  }
+}
