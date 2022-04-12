a2b726f1bbef32ba65dd39f8c6440d9b534a44fb
Lqf <leiqifan@163.com>
2022-04-12 16:59:39 +0800

    LRU缓存

--- scripts/utils/log.js
+++ scripts/utils/log.js
@@ -5,10 +5,10 @@ const chalk = require('chalk')
 const log = (msg, color = 'green') => {
   if (!chalk[color])
     color = 'green'
-  console.log(chalk[color].msg)
+  console.log(chalk[color](msg))
 }
 
-const figletLog = async(text, color = 'green') => {
+const figletLog = async (text, color = 'green') => {
   const msg = await figlet(text)
   log(msg, color)
 }
