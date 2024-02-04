21fd7d3c106f269f493aa2d957be8fc4150c1b37
fanmao <leiqifan@163.com>
2024-02-04 14:31:40 +0800

    feat: leetcode 292

---
+++ src/292.Nim游戏/index.spec.ts
@@ -0,0 +1,7 @@
+import { canWinNim } from '.'
+
+describe('Nim游戏', () => {
+    it('示例', () => {
+        expect(canWinNim(4)).toBe(false)
+    })
+})
---
+++ src/292.Nim游戏/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @description: 时间复杂度 O(!) 空间复杂度 O(1)
+ * @return {*}
+ * @param {number} n
+ */
+export function canWinNim(n: number): boolean {
+    return n % 4 !== 0
+}
